fb7f0d822c8421a3b1013dac2d7e5cc4be10278c
minhduongt <80615805+minhduongt@users.noreply.github.com>
2022-01-24 01:00:12 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
-//import { useState } from 'react';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import { Button, TextField } from '@mui/material';
 import { styled } from '@mui/system';
 import FormTodo from './components/FormTodo';
 
@@ -15,46 +13,19 @@ const HeaderStyled = styled('header')(
 
 function App() {
 
-  //const [name, setName] = useState('');
-
-  // function changeUserName(e) {
-  //   setName({ name: e.target.value });
-  // }
-
-  // function displayChangeName() {
-  //   document.getElementById('nameTextField').style.display = "block"
-  // }
-
-
-
   return (
     <>
 
       <Container maxWidth="sm">
         <Box sx={{ bgcolor: 'lightgrey', height: '100vh' }}>
           <div>
-            <h3>
-              Welcome, Guest
-              <Button
-
-                style={{
-                  float: 'right'
-                }}
-              >Change Name
-              </Button>
-              <TextField
-                id='nameTextField'
-                size='small'
-                placeholder='Enter your name'
-                style={{
-                  float: 'right',
-                  display: 'none'
-
-                }}
-              >
-              </TextField>
-            </h3>
-
+            <p
+              style={{
+                textAlign: 'center'
+              }}
+            >
+              Make your own list of things to deal with
+            </p>
             <br></br>
             <HeaderStyled>
               Todo List
